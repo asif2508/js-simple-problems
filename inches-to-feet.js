f3d03a121743fa7cf335e2f5bca14094f2cb3dba
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-01-29 22:00:45 +0600

    first commit

---
+++ inches-to-feet.js
@@ -0,0 +1,43 @@
+//inches to feet
+function InchesTOFeet(inches){
+    let feet = inches/12;
+    return inches +" inches = "+feet+" feet.";
+}
+//miles to kilometres
+function milesToKilo(miles){
+    let kilo = miles * 1.609;
+    kilo = kilo.toFixed(2);
+    return miles + " miles = "+kilo+" kilometres.";
+}
+
+//even and odd number
+function evenAndOdd(number){
+    if(number % 2 == 0){
+        console.log("The number is even");
+    }else{
+        console.log("The number is odd");
+    }
+}
+
+//leapyear
+function isLeapYear(year){
+    if(year % 400 == 0 && year % 100 == 0){
+        console.log(year +" is a leap-year");
+    }
+    else if(year % 4 == 0 && year % 100 != 0){
+        console.log(year +" is a leap-year.");
+    }
+    else{
+        console.log(year + " is not a leap-year.")
+    }
+}
+
+//factorial using for loop
+function Factorial(number){
+    let newnumber = number;
+    for(let i = 1; i < number; i++){
+        newnumber = newnumber * i;
+        console.log(newnumber);
+    }
+}
+Factorial(10);
